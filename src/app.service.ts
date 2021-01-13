aa9b9049035ec1dec093ad70d8943ecc7c50cbe4
Magz8984 <collinsmuthinja@gmail.com>
2021-01-13 13:48:13 +0300

    Make minor changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -45,7 +45,7 @@ export class AppService {
   getUsers() {
     return new Promise((res, rej) => {
       // Fetch users from the ./users.json file
-      fs.readFile('src/users.json', 'utf8', (err: any, jsonString: string) => {
+      fs.readFile('./users.json', 'utf8', (err: any, jsonString: string) => {
         if (err) {
             rej(err)
             return;
